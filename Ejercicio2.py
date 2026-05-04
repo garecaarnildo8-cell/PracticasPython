cb406aa02a6a8775b7624d14bab30e0f2660d378
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-05-03 20:24:36 -0400

    Add validation for gasto against sueldo

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -7,7 +7,9 @@ gasto = int(input("Cuanto gasta por semana: "))
 if gasto<=0 :
     print("Error el gasto no puede ser negativo.....")
     exit()
-
+if gasto>=sueldo: 
+    print ("El gasto no puede ser mayor que el sueldo")
+    exit()
 duracion = sueldo//gasto
 
 print("Los dias que durara su saldo es de: ",duracion)
