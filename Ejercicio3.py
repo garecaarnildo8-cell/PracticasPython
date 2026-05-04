14b7c3b814c1c1a9602f6cae15eb57f90685eb8a
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-05-04 19:55:14 -0400

    Ejercicio3 y 4: bucles par e impar y suma

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+print("Pares: ")
+for i in range(1,21):
+    if i%2==0:
+        print(i)
+
+print("Impares: ")
+
+for i in range(1,21):
+    if i%2==1:
+        print(i)
+
---
+++ Ejercicio4.py
@@ -0,0 +1,5 @@
+suma=0
+for i in range(1,101): 
+    suma = suma+i
+    
+print("La suma hasta el numero 100 es: ",suma)
