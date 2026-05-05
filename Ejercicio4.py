1655aa018fb501e6db02c556afb54258d8c8c123
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-05-04 20:51:22 -0400

    Ejercicio5 : Suma de numeros con while

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -2,4 +2,4 @@ suma=0
 for i in range(1,101): 
     suma = suma+i
     
-print("La suma hasta el numero 100 es: ",suma)
+print("La suma hasta el numero 100 es:",suma)
---
+++ Ejercicio5.py
@@ -0,0 +1,9 @@
+numero = int(input("Ingrese un numero: "))
+suma = numero
+while numero!=0: 
+    
+    numero = int(input("Ingrese un numero: "))
+    if(numero !=0): 
+        suma = suma +numero
+
+print("La suma es: ",suma)
