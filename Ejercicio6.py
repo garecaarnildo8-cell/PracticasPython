56f2dc0b4a600eb845930a667417784f956fd9bd
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-05-04 21:08:15 -0400

    Ejercicio6: Adivina el numero secreto con while

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,1 +1,14 @@
-numero = int(input())
+numero_secreto=7
+numero = int(input("Diga un numero y adivine el numero secreto: "))
+while numero !=numero_secreto: 
+    
+    if numero<numero_secreto:
+        print("El numero es menor al numero secreto.")
+    
+    if numero>numero_secreto: 
+        print("El numero es mayor al numero secreto.")
+        
+    numero = int(input("Diga un numero y adivine el numero secreto: "))
+
+if numero==numero_secreto :
+        print("Ganaste....")
