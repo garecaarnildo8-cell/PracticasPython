df77158de20f5368c46bffb0a9ffe76e92257b5d
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-05-04 21:21:57 -0400

    Update Ejercicio6.py with new guessing game logic

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,14 +1,12 @@
-numero_secreto=7
+numero_secreto = 7
 numero = int(input("Diga un numero y adivine el numero secreto: "))
-while numero !=numero_secreto: 
-    
-    if numero<numero_secreto:
+
+while numero != numero_secreto: 
+    if numero < numero_secreto:
         print("El numero es menor al numero secreto.")
-    
-    if numero>numero_secreto: 
+    elif numero > numero_secreto: 
         print("El numero es mayor al numero secreto.")
         
-    numero = int(input("Diga un numero y adivine el numero secreto: "))
+    numero = int(input("Diga un numero y adivine el numero secreto: " ))
 
-if numero==numero_secreto :
-        print("Ganaste....")
+print("¡Ganaste!")
